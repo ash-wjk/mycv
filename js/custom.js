9a065a1a683fd568d4a4ace33ce68dafbcdc6c2a
Asha Wijekoon <Asha@Nuwans-Air.home>
2017-01-22 22:38:05 +0000

    Added skills

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+
+(function($) { 
+	"use strict";
+
+
+// for skill chat jquary
+$(document).ready(function(e) {
+//var windowBottom = $(window).height();
+var index=0;
+$(document).scroll(function(){
+	var top = $('.technical').height()-$(window).scrollTop();
+	console.log(top)
+	if(top<-300){
+		if(index==0){	
+			
+			$('.chart').easyPieChart({
+				easing: 'easeOutBounce',
+				onStep: function(from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent));
+				}
+			});
+			
+		}
+		index++;
+	}
+})
+//console.log(nagativeValue)
+});
+
+
+
+
+// Somth page scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -60
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+// chart loding
+$(window).load(function() {
+	
+	var chart = window.chart = $('.chart').data('easyPieChart');
+	$('.js_update').on('click', function() {
+		chart.update(Math.random()*100);
+	});
+});
+
+
+}(jQuery));
